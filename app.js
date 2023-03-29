b94ff8515c927c9dc80da87bb9c0d6664ea31d42
Barış Yarar <45574659+BarisDev@users.noreply.github.com>
2023-03-30 01:31:41 +0300

    origin updated.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-        origin: "https://bycanvas.web.app/"
+        origin: ["https://bycanvas.web.app/", "http://localhost:4200"]
     }
 });
 
