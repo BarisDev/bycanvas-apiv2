c5079f5171ca7c4fd66990423adf952ec3f9955e
Barış Yarar <45574659+BarisDev@users.noreply.github.com>
2023-03-30 02:52:43 +0300

    cors test

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const http = require('http');
+var cors = require('cors')
+app.use(cors());
+
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
